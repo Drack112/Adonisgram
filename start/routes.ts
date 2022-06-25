e2fe340e3cc6cf484d3cfcb0b01cd54bfd214485
Drack112 <onepessoa3@gmail.com>
2022-06-25 12:41:47 -0300

    Setup Routes and templates 📎

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+Route.on("/").render("welcome")
+Route.on("/signup").render("auth/signup")
+Route.on("/login").render("auth/login")
