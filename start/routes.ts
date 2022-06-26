b81a933d849a67f7e5598423be4d34ecf36978dd
Drack112 <onepessoa3@gmail.com>
2022-06-26 15:04:09 -0300

    Routes

--- start/routes.ts
+++ start/routes.ts
@@ -20,11 +20,27 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.on('/').render('welcome')
-Route.on('/signup').render('auth/signup')
-Route.on('/login').render('auth/login')
-Route.on('/profile').render('profile').middleware('auth')
+Route.get('/', 'HomeController.index')
 
+// Auth Routes
+Route.on('/signup').render('auth/signup').middleware('guest')
+Route.on('/login').render('auth/login').middleware('guest')
 Route.post('/signup', 'AuthController.signup')
 Route.post('/login', 'AuthController.login')
 Route.post('/logout', 'AuthController.logout')
+
+// Email Routes
+Route.get('/verify-email/:email', 'EmailVerifiesController.confirm').as('verifyEmail')
+Route.post('/verify-email', 'EmailVerifiesController.index').middleware('auth')
+
+// Post Routes
+Route.get('/posts/create', 'PostsController.create').middleware('auth')
+Route.post('/posts/create', 'PostsController.store').middleware('auth')
+
+// Routes of people
+Route.post('/follow/:userid', 'FollowsController.store').middleware('auth')
+Route.delete('/follow/:userid', 'FollowsController.destroy').middleware('auth')
+
+Route.get('/accounts/edit', 'ProfilesController.edit').middleware('auth')
+Route.post('/accounts/edit', 'ProfilesController.update').middleware('auth')
+Route.get('/:username', 'ProfilesController.index').middleware('auth')
