9460b9086d9b982f912eddb7b4c01b3629499f0a
Drack112 <onepessoa3@gmail.com>
2022-06-26 13:28:17 -0300

    Setup mailer 📩

--- database/migrations/1656175148561_users.ts
+++ database/migrations/1656175148561_users.ts
@@ -8,6 +8,7 @@ export default class extends BaseSchema {
       table.increments('id')
       table.string('name')
       table.string('email')
+      table.datetime('email_verified_at').nullable()
       table.string('password')
       table.timestamps(true)
     })
